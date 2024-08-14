64f6eff2205dc46509c2ecc7c870c8475adfe560
naufaltsani <naufaltsani03.nt@gmail.com>
2024-08-14 20:33:17 +0700

    Update Content Configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./**/*.{html,js}'],
+  content:
+    ['index.html',
+      './**/*.{html,js}'
+    ],
   theme: {
     extend: {},
   },
